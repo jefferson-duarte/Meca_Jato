7a7d67908193efe004b69a6644fbde072fdde29b
Jefferson Duarte <jeffersonsduarte@live.com>
2022-09-17 12:09:07 +0100

    Criado models Carro e Cliente

--- clientes/views.py
+++ clientes/views.py
@@ -1,5 +1,40 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Cliente, Carro
+import re
 
 
 def clientes(request):
-    return render(request, 'clientes.html')
+    if request.method == 'GET':
+        return render(request, 'clientes.html')
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+        carros = request.POST.getlist('carro')
+        placas = request.POST.getlist('placa')
+        anos = request.POST.getlist('ano')
+
+        cliente = Cliente.objects.filter(cpf=cpf)
+
+        if cliente.exists():
+            return render(request, 'clientes.html', {'nome': nome, 'sobrenome': sobrenome, 'email': email, 'carros': zip(carros, placas, anos)})  # noqa
+
+        if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):  # noqa
+            return render(request, 'clientes.html', {'nome': nome, 'sobrenome': sobrenome, 'cpf': cpf, 'carros': zip(carros, placas, anos)})  # noqa
+
+        cliente = Cliente(
+            nome=nome,
+            sobrenome=sobrenome,
+            email=email,
+            cpf=cpf
+        )
+
+        cliente.save()
+
+        for carro, placa, ano in zip(carros, placas, anos):
+            car = Carro(carro=carro, placa=placa, ano=ano, cliente=cliente)
+            car.save()
+
+    return HttpResponse('Teste')
