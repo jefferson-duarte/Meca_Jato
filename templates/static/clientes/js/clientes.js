fa8b7abf870ec156d29299899f353e1c9ad5c027
Jefferson Duarte <jeffersonsduarte@live.com>
2022-09-18 11:13:37 +0100

    Adicionado funcao atualiza cliente

--- templates/static/clientes/js/clientes.js
+++ templates/static/clientes/js/clientes.js
@@ -37,7 +37,18 @@ function dados_cliente() {
     }).then(function(result) {
         return result.json()
     }).then(function(data) {
-        console.log(data)
+        document.getElementById('form-att-cliente').style.display = 'block'
+
+        nome = document.getElementById('nome')
+        nome.value = data['nome']
+        
+        sobrenome = document.getElementById('sobrenome')
+        sobrenome.value = data['sobrenome']
+        
+        email = document.getElementById('email')
+        email.value = data['email']
+        
+        cpf = document.getElementById('cpf')
+        cpf.value = data['cpf']
     })
-    
 }
